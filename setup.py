6380d831e7032460673530542fb029e7a3e6a24f
perillaroc <perillaroc@gmail.com>
2018-02-12 10:28:56 +0800

    add Dockerfile for slurm_partition.

--- setup.py
+++ setup.py
@@ -46,8 +46,6 @@ setup(
         'test': ['pytest'],
     },
 
-    dependency_links=[path.join(here, 'vendor/nwpc-hpc-model')],
-
     entry_points={
         'console_scripts': [
             'disk_space_exporter=nwpc_hpc_exporter.disk_space.exporter:main',
