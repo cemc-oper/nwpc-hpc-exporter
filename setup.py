75dc2dddfbb96c628b7122badfcd7dcd19d443d7
perillaroc <perillaroc@gmail.com>
2018-07-23 13:23:17 +0800

    update config file for AIX disk space.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='nwpc-hpc-exporter',
 
-    version='0.1.1',
+    version='0.2.0',
 
     description='An HPC exporter for NWPC',
     long_description=long_description,
